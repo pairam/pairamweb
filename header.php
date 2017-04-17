aa827061a9e5fb5035cf028385531ebfaa171502
johnny <johnny@psytrance-radio.cz>
2017-04-17 20:50:57 +0200

    added conversion code to header.php template

--- header.php
+++ header.php
@@ -36,20 +36,41 @@
 		<!-- End Facebook Pixel Code -->
 	</head>
 	<body <?php body_class(); ?>>
+
+		<!-- Google Code for Konverze Popt&aacute;vka Conversion Page -->
+		<script type="text/javascript">
+			/* <![CDATA[ */
+			var google_conversion_id = 925411765;
+			var google_conversion_language = "en";
+			var google_conversion_format = "3";
+			var google_conversion_color = "ffffff";
+			var google_conversion_label = "3LGVCOy55m8QtdOiuQM";
+			var google_remarketing_only = false;
+			/* ]]> */
+		</script>
+		<script type="text/javascript"  
+			src="//www.googleadservices.com/pagead/conversion.js">
+		</script>
+		<noscript>
+			<div style="display:inline;">
+			<img height="1" width="1" style="border-style:none;" alt=""  
+			src="//www.googleadservices.com/pagead/conversion/925411765/?label=3LGVCOy55m8QtdOiuQM&amp;guid=ON&amp;script=0"/>
+			</div>
+		</noscript>
+
 		<?php //if (is_active_sidebar('top')) dynamic_sidebar('top'); ?>
 		<?php //wp_nav_menu(array('theme_location' => 'primary', 'container' => false)); ?>
 		<div class="wrapper">
 		<div class="replaceHeader"></div>
 		<header>
 			<div class="inner">
-                                <h1><a href="/<?php echo qtranxf_getLanguage(); ?>"><img class="logo" src="<?php bloginfo('template_url'); ?>/images/<?php if (qtranxf_getLanguage()=='en') { echo 'logo_en.png'; } else { echo 'logo.png'; } ?>" alt="Pairam" /></a></h1>
+                <h1><a href="/<?php echo qtranxf_getLanguage(); ?>"><img class="logo" src="<?php bloginfo('template_url'); ?>/images/<?php if (qtranxf_getLanguage()=='en') { echo 'logo_en.png'; } else { echo 'logo.png'; } ?>" alt="Pairam" /></a></h1>
 				<?php wp_nav_menu(array('theme_location' => 'primary', 'container' => false)); ?>
 				<div class="menus">
 					<?php wp_nav_menu(array('theme_location' => 'Secondary', 'container' => false)); ?>
 					<?php if (is_active_sidebar('lang')) dynamic_sidebar('lang'); ?>
 				</div>
 				<img id="openMenu" class="mobile" src="<?php bloginfo('template_url'); ?>/images/menu.png" alt="Menu" />
-
 
 				<div id="menuMobile">
 					<?php wp_nav_menu(array('theme_location' => 'primary', 'container' => false)); ?>
